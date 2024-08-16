5d9bece66299fe3f3af97dcd428e6c64333742b4
So5L <89294084+So5L@users.noreply.github.com>
2024-08-16 22:27:43 +0900

    24.8.16
    just for backup

--- main.go
+++ main.go
@@ -1,9 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+var errRequestFailed = errors.New("Request failed")
 
 func main() {
-	a := 2
-	b := &a
-	fmt.Println(*b)
+	urls := []string{
+		go "https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
+	}
+	for _, url := range urls {
+		hitURL(url)
+	}
+}
+
+func hitURL(url string) error {
+	fmt.Println("Checking:", url)
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode >= 400 {
+		return errRequestFailed
+	}
+	return nil
 }
